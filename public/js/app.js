cebdc1e87429ee069f9de4a67eac5fc804df5ad8
Bojan <bojan.rogalo@gmail.com>
2017-11-25 18:32:54 +0100

    Refactor and enhance filtering.

--- public/js/app.js
+++ public/js/app.js
@@ -3,47 +3,31 @@ $(document).foundation()
 $(document).ready(function() {
 
   $('#instances').DataTable({
-    "mark": true,
-    "pageLength": 50,
-    "columnDefs": [ {
-      "targets"  : 'no-sort',
-      "orderable": false,
+    'mark': true,
+    'pageLength': 50,
+    'columnDefs': [ {
+      'targets'  : 'no-sort',
+      'orderable': false,
     }]
   });
 
-  $("a#toggle-tags").click(function(){
-    $(this).parent().parent().children(".label.tag.other").toggle();
+  $('a#toggle-tags').click(function(){
+    $(this).parent().parent().children('.label.tag.other').toggle();
+    if($(this).text() == 'All'){
+      $(this).text('Filtered');
+    } else {
+      $(this).text('All');
+    }
   });
 
-  $("a#show-all-tags").click(function(){
-    $(".label.tag.other").show();
+  $('a#show-all-tags').click(function(){
+    $('.label.tag.other').show();
+    $('.label.toggle-link > a#toggle-tags').text('Filtered')
   });
 
-  $("a#show-filtered-tags").click(function(){
-    $(".label.tag.other").hide();
+  $('a#show-filtered-tags').click(function(){
+    $('.label.tag.other').hide();
+    $('.label.toggle-link > a#toggle-tags').text('All')
   });
 
-  $('#user_change_password').submit(function() {
-    var new_password = $.trim($('input[name="new_password"]').val())
-    var new_password_confirmation = $.trim($('input[name="new_password_confirmation"]').val())
-
-    if (new_password == '') {
-      alert('Password cannot be empty or blank')
-      return false
-    }
-
-    if (new_password != new_password_confirmation) {
-      alert('New password and confirmation do not match')
-      return false
-    }
-  })
-
-  $('#user_delete').submit(function() {
-    var admin_password = $.trim($('input[name="admin_password"]').val())
-    if (admin_password == '') {
-      alert('Password cannot be empty or blank')
-      return false
-    }
-    return confirm('Are you sure you want to delete this user?')
-  })
 })
