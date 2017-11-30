099914230659493a35bb5467fe2aca58c794605c
Bojan <bojan.rogalo@gmail.com>
2017-11-30 01:02:03 +0100

    Add RDS Instances.

--- db.rb
+++ db.rb
@@ -39,6 +39,10 @@ class Vpc
     :parent_key => [ :vid ], # local to this model (Vpc)
     :child_key  => [ :vpc_id ]  # in the remote model (Instance)
 
+  has n, :db_instances, 'DbInstance',
+    :parent_key => [ :vid ], # local to this model (Vpc)
+    :child_key  => [ :vpc_id ]  # in the remote model (DbInstance)
+
   has n, :subnets, 'Subnet', :parent_key => [ :vid ], :child_key => [ :vpc_id ]
 end
 
@@ -56,6 +60,22 @@ class Subnet
 
   belongs_to :vpc, 'Vpc', :parent_key => [ :vid ], :child_key  => [ :vpc_id ]
   has n, :instances, 'Instance', :parent_key => [ :sid ], :child_key  => [ :subnet_id ]
+end
+
+class DbInstance
+  include DataMapper::Resource
+  property :id, Serial
+  property :dbiid, String
+  property :name, String
+  property :db_instance_class, String
+  property :address, String
+  property :vpc_id, String
+  property :availability_zone, String
+  property :secondary_availability_zone, String
+
+  property :resource_json, Text
+
+  belongs_to :vpc, 'Vpc', :parent_key => [ :vid ], :child_key  => [ :vpc_id ]
 end
 
 # Load DB on startup
@@ -83,6 +103,7 @@ def reload_aws_resources
   ENV['AWS_REGION'] ||= 'eu-central-1'
   LOG.info "AWS Region: #{ENV['AWS_REGION']}"
   ec2 = Aws::EC2::Client.new(region: ENV['AWS_REGION'])
+  rds = Aws::RDS::Client.new(region: ENV['AWS_REGION'])
 
   reservations = ec2.describe_instances.reservations
   reservations.each do |reservation|
@@ -124,6 +145,20 @@ def reload_aws_resources
                   resource_json: resource_json
   end
   LOG.info "#{subnets.count} Subnet(s) were loaded"
+
+  db_instances = rds.describe_db_instances.db_instances
+  db_instances.each do |db_instance|
+    resource_json = db_instance.to_h.to_json
+    DbInstance.create dbiid: db_instance.dbi_resource_id,
+                      name: db_instance.db_instance_identifier,
+                      vpc_id: db_instance.db_subnet_group.vpc_id,
+                      db_instance_class: db_instance.db_instance_class,
+                      address:  db_instance.endpoint.address,
+                      availability_zone: db_instance.availability_zone,
+                      secondary_availability_zone: db_instance.secondary_availability_zone,
+                      resource_json: resource_json
+  end
+  LOG.info "#{db_instances.count} RDS DB Instance(s) were loaded"
 
   LOG.info "Loading AWS Resources done"
 end
@@ -185,3 +220,53 @@ end
 #  instance_tenancy="default",
 #  is_default=false,
 #  ipv_6_cidr_block_association_set=[]>#
+#
+#
+# Aws> rds.describe_db_instances[:db_instances][1].to_h
+# [Aws::RDS::Client 200 0.498392 0 retries] describe_db_instances()
+# => {:db_instance_identifier=>"stage-rancher-server",
+#  :db_instance_class=>"db.t2.micro",
+#  :engine=>"mysql",
+#  :db_instance_status=>"available",
+#  :master_username=>"rancher",
+#  :db_name=>"rancher",
+#  :endpoint=>{:address=>"stage-rancher-server.clrzznqs5nb4.eu-central-1.rds.amazonaws.com", :port=>3306, :hosted_zone_id=>"Z1RLNUO7B9Q6NB"},
+#  :allocated_storage=>5,
+#  :instance_create_time=>2017-11-29 16:30:13 UTC,
+#  :preferred_backup_window=>"01:37-02:07",
+#  :backup_retention_period=>7,
+#  :db_security_groups=>[],
+#  :vpc_security_groups=>[{:vpc_security_group_id=>"sg-0d660067", :status=>"active"}],
+#  :db_parameter_groups=>[{:db_parameter_group_name=>"default.mysql5.6", :parameter_apply_status=>"in-sync"}],
+#  :availability_zone=>"eu-central-1c",
+#  :db_subnet_group=>
+#   {:db_subnet_group_name=>"stage",
+#    :db_subnet_group_description=>"Data subnet group for stage",
+#    :vpc_id=>"vpc-hha748d8",
+#    :subnet_group_status=>"Complete",
+#    :subnets=>
+#     [{:subnet_identifier=>"subnet-300aba72", :subnet_availability_zone=>{:name=>"eu-central-1a"}, :subnet_status=>"Active"},
+#      {:subnet_identifier=>"subnet-3000611e3", :subnet_availability_zone=>{:name=>"eu-central-1b"}, :subnet_status=>"Active"},
+#      {:subnet_identifier=>"subnet-3005d309", :subnet_availability_zone=>{:name=>"eu-central-1c"}, :subnet_status=>"Active"}]},
+#  :preferred_maintenance_window=>"thu:03:05-thu:03:35",
+#  :pending_modified_values=>{},
+#  :latest_restorable_time=>2017-11-29 22:40:00 UTC,
+#  :multi_az=>true,
+#  :engine_version=>"5.6.37",
+#  :auto_minor_version_upgrade=>true,
+#  :read_replica_db_instance_identifiers=>[],
+#  :license_model=>"general-public-license",
+#  :option_group_memberships=>[{:option_group_name=>"default:mysql-5-6", :status=>"in-sync"}],
+#  :secondary_availability_zone=>"eu-central-1b",
+#  :publicly_accessible=>false,
+#  :storage_type=>"gp2",
+#  :db_instance_port=>0,
+#  :storage_encrypted=>false,
+#  :dbi_resource_id=>"db-YTKKUN354HHHIBKFJE6AHYME4A",
+#  :ca_certificate_identifier=>"rds-ca-2015",
+#  :domain_memberships=>[],
+#  :copy_tags_to_snapshot=>false,
+#  :monitoring_interval=>0,
+#  :db_instance_arn=>"arn:aws:rds:eu-central-1:000270007572:db:stage-rancher-server",
+#  :iam_database_authentication_enabled=>false,
+#  :performance_insights_enabled=>false}
